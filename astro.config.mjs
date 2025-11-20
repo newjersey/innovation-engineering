ecb09101df32b3bdd5c4341f179a187b297f633d
Anne LoVerso <anne.loverso@gmail.com>
2025-11-20 14:22:54 -0500

    add base url to links for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://newjersey.github.io',
+	base: "/innovation-engineering",
 	integrations: [
 		starlight({
 			title: '🤖 OOI Engineering',
--- src/content/docs/wiki-meta/adding-docs.md
+++ src/content/docs/wiki-meta/adding-docs.md
@@ -3,4 +3,6 @@ title: Adding new docs
 description: Process for adding and requesting
 ---
 
+Note: make sure links have the `/innovation-engineering` prefix
+
 To be filled in
