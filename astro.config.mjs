73cba956e2959b8bf6d0ac58cf7275521e9bea13
Spencer Wohlers <4006420+seidior@users.noreply.github.com>
2026-05-27 14:58:53 -0400

    chore: fixes for Starlight 0.39 and above

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,94 +1,91 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightLinksValidator from 'starlight-links-validator'
+
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+import starlightLinksValidator from "starlight-links-validator";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://newjersey.github.io',
-	base: "/innovation-engineering",
-	redirects: {
-		'/guides/claude-code-bedrock/': '/guides/development/claude-code-bedrock/',
-	},
-	integrations: [
-		starlight({
-			plugins: [starlightLinksValidator()],
-			title: 'NJIA Engineering',
-			logo: {
-				src: './src/assets/robot.png',
-			},
-			customCss: [
-				// Fontsource files for to regular, semi-bold, and bold font weights.
-				"@fontsource/public-sans/latin.css",
-				"@fontsource/public-sans/latin-italic.css",
-				"./src/styles/custom.css",
-			],
-			components: {
-				MarkdownContent: './src/components/MarkdownContent.astro',
-			},
-			lastUpdated: true,
-			editLink: {
-				baseUrl: 'https://github.com/newjersey/innovation-engineering/edit/main/',
-			},
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/newjersey/innovation-engineering' }],
-			sidebar: [
-				{
-					label: 'Meta',
-					autogenerate: { directory: 'meta' },
-				},
-				{
-					label: 'Guides',
-					items: [
-						{
-							label: 'Getting Started',
-							collapsed: false,
-							autogenerate: { 
-								directory: 'guides/getting-started',
-							},
-						},
-						{
-							label: 'Agency Collaboration',
-							collapsed: true,
-							autogenerate: { 
-								directory: 'guides/agency-collaboration',
-							},
-						},
-						{
-							label: 'Development',
-							collapsed: true,
-							autogenerate: { 
-								directory: 'guides/development',
-							},
-						},
-						{
-							label: 'Github Actions',
-							collapsed: true,
-							autogenerate: { 
-								directory: 'guides/github-actions',
-							},
-						},
-						{
-							label: 'Monitoring & Analytics',
-							collapsed: true,
-							autogenerate: {
-								directory: 'guides/monitoring',
-							},
-						},
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-				{
-					label: 'Tools we use',
-					autogenerate: { directory: 'tech-recommendations' },
-				},
-				{
-					label: 'Onboarding',
-					autogenerate: { directory: 'onboarding' },
-				},
-			],
-		}),
-	],
+  site: "https://newjersey.github.io",
+  base: "/innovation-engineering",
+  redirects: {
+    "/guides/claude-code-bedrock/": "/guides/development/claude-code-bedrock/",
+  },
+  integrations: [
+    starlight({
+      plugins: [starlightLinksValidator()],
+      title: "NJIA Engineering",
+      logo: {
+        src: "./src/assets/robot.png",
+      },
+      customCss: [
+        // Fontsource files for to regular, semi-bold, and bold font weights.
+        "@fontsource/public-sans/latin.css",
+        "@fontsource/public-sans/latin-italic.css",
+        "./src/styles/custom.css",
+      ],
+      components: {
+        MarkdownContent: "./src/components/MarkdownContent.astro",
+      },
+      lastUpdated: true,
+      editLink: {
+        baseUrl: "https://github.com/newjersey/innovation-engineering/edit/main/",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/newjersey/innovation-engineering",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Meta",
+          items: [{ autogenerate: { directory: "meta" } }],
+        },
+        {
+          label: "Guides",
+          items: [
+            {
+              label: "Getting Started",
+              collapsed: false,
+              items: [{ autogenerate: { directory: "guides/getting-started" } }],
+            },
+            {
+              label: "Agency Collaboration",
+              collapsed: true,
+              items: [{ autogenerate: { directory: "guides/agency-collaboration" } }],
+            },
+            {
+              label: "Development",
+              collapsed: true,
+              items: [{ autogenerate: { directory: "guides/development" } }],
+            },
+            {
+              label: "Github Actions",
+              collapsed: true,
+              items: [{ autogenerate: { directory: "guides/github-actions" } }],
+            },
+            {
+              label: "Monitoring & Analytics",
+              collapsed: true,
+              items: [{ autogenerate: { directory: "guides/monitoring" } }],
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          items: [{ autogenerate: { directory: "reference" } }],
+        },
+        {
+          label: "Tools we use",
+          items: [{ autogenerate: { directory: "tech-recommendations" } }],
+        },
+        {
+          label: "Onboarding",
+          items: [{ autogenerate: { directory: "onboarding" } }],
+        },
+      ],
+    }),
+  ],
 });
